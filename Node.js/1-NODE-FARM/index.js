02ea7a3147f10123888dcffcbde3b43a96919659
Gabriel Ferreira <vieira.ferreira@unesp.br>
2024-05-06 10:48:54 -0300

    :construction: in progress: Section 2 - finished

--- Node.js/1-NODE-FARM/index.js
+++ Node.js/1-NODE-FARM/index.js
@@ -209,6 +209,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -217,6 +218,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
+// console.log(slugify('Fresh Avocado', { lowercase: true }))
+// console.log(slugify('Fresh Avocado', { lower: true }))
 
 const server = http.createServer((req, res) => {
     // console.log(req.url);
