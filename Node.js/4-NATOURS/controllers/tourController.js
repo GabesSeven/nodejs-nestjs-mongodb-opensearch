65d8e300bf6b6689376c85714e0fc186d168de85
GabesSeven <vieira.ferreira@unesp.br>
2024-05-10 10:35:40 -0300

    :construction: in progress: Node.js - Section 7 - in progress

---
+++ Node.js/4-NATOURS/controllers/tourController.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+/* Local File Database */
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+/* Middlewares */
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
+/* Route Handlers */
+exports.getAllTours = (req, res) => {
+    // console.log(req.requestTime);
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours // tours: tours
+        }
+    });
+}
+
+exports.getTour = (req, res) => {
+    // console.log(req.params);
+    const id = req.params.id * 1; // convert to a number
+    const tour = tours.find(el => el.id === id); 
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour // tour: tour
+        }
+    });
+}
+
+exports.createTour = (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,  
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }        
+            });
+        }
+    );
+}
+
+exports.updateTour = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Update tour here...>'
+        }
+    });
+}
+
+exports.deleteTour = (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+}
